283b48e4314a0843340abfe7d6888a4a0ee6b4b0
Kinesis <lordkinesis@naver.com>
2024-05-16 10:47:43 +0900

    Create UserData.py

--- src/Hello.py
+++ src/Hello.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QMainWindow
+from UserData import Users
 
 class SayHello(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.user = Users()
 
     def SayingHello(self, window): # QMainWindow 인스턴스를 window로 받음
-        window.statusBar().showMessage("안녕!")
+        window.statusBar().showMessage(f"안녕하세요, {self.user.name}!")
---
+++ src/UserData.py
@@ -0,0 +1,6 @@
+class Users:
+    def __init__(self):
+        self.name = "Master"
+        self.age = "19"
+        self.school = "광운대학교"
+        
